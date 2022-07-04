a4001214483a66b7dfdffc6ed0c405ba44529576
Jacob Templeton <jwctempleton@gmail.com>
2022-07-03 21:36:44 -0700

    Add state for questions, set state with API call

--- quizzler/src/App.js
+++ quizzler/src/App.js
@@ -1,13 +1,39 @@
+import React from "react";
 import "./App.css";
 import Welcome from "./Welcome";
 
 function App() {
+  const [startQuiz, setStartQuiz] = React.useState(false);
+
+  const [quizData, setQuizData] = React.useState([]);
+
+  function toggleStart() {
+    setStartQuiz((prev) => !prev);
+  }
+
+  React.useEffect(() => {
+    fetch(
+      `https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple`
+    )
+      .then((res) => res.json())
+      .then((data) => setQuizData(data));
+  }, []);
+
+  console.log(quizData);
   return (
     <div className="App">
       <header className="header">
         <h1 className="header-title">Quizzler</h1>
       </header>
-      <Welcome />
+      {!startQuiz && (
+        <div className="welcome">
+          <Welcome />
+          <button className="welcome-button" onClick={toggleStart}>
+            Start Quiz
+          </button>
+        </div>
+      )}
+      {startQuiz && <h2>Start Quiz</h2>}
     </div>
   );
 }
--- quizzler/src/Welcome.js
+++ quizzler/src/Welcome.js
@@ -2,7 +2,6 @@ export default function Welcome() {
   return (
     <div className="welcome">
       <h2>Welcome to the Quizzler App</h2>
-      <button className="welcome-button">Start Quiz</button>
     </div>
   );
 }
