a082f5d186fd336afe22a63bb9cccbef586c9e05
Jacob Templeton <jwctempleton@gmail.com>
2022-07-10 16:12:05 -0700

    Add element to map through JSON response, display each question on screen, also replace HTML elements with their correct character

--- quizzler/src/Questions.js
+++ quizzler/src/Questions.js
@@ -1,8 +1,12 @@
 export default function Questions(props) {
   console.log(props.quizData);
-  return (
-    <div>
-      <p>{props.quizData.results[0].question}</p>
-    </div>
-  );
+  const quizElement = props.quizData.results.map((quiz) => (
+    <p>
+      {quiz.question
+        .replace(/(&quot;)/g, '"')
+        .replace(/(&amp;)/g, "&")
+        .replace(/(&#039;)/g, "'")}
+    </p>
+  ));
+  return <div>{quizElement}</div>;
 }
