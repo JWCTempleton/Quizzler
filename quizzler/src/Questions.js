84a8ce28e451c5002343c4228610f988f48fd003
Jacob Templeton <jwctempleton@gmail.com>
2022-07-13 14:04:17 -0700

    Add buttons for possible answers to questions

--- quizzler/src/Questions.js
+++ quizzler/src/Questions.js
@@ -1,12 +1,30 @@
 export default function Questions(props) {
   console.log(props.quizData);
-  const quizElement = props.quizData.results.map((quiz) => (
-    <p>
-      {quiz.question
-        .replace(/(&quot;)/g, '"')
-        .replace(/(&amp;)/g, "&")
-        .replace(/(&#039;)/g, "'")}
-    </p>
-  ));
+  const quizElement = props.quizData.results.map((quiz) => {
+    const questionArray = [];
+    questionArray.push(quiz.correct_answer);
+    questionArray.push(...quiz.incorrect_answers);
+
+    return (
+      <div>
+        <p key={quiz.question}>
+          {quiz.question
+            .replace(/(&quot;)/g, '"')
+            .replace(/(&amp;)/g, "&")
+            .replace(/(&#039;)/g, "'")}
+        </p>
+        <div>
+          {questionArray.map((answer) => (
+            <button key={answer}>
+              {answer
+                .replace(/(&quot;)/g, '"')
+                .replace(/(&amp;)/g, "&")
+                .replace(/(&#039;)/g, "'")}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  });
   return <div>{quizElement}</div>;
 }
