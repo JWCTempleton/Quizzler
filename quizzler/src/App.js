ab1c90f3c6255e03df0628ad8d7f0bf3a5e1d327
Jacob Templeton <jwctempleton@gmail.com>
2022-07-20 20:19:03 -0700

    Add changed background color to button if the answer has been selected (just based on prop data, function to change state not added)

--- quizzler/src/App.js
+++ quizzler/src/App.js
@@ -33,7 +33,7 @@ function App() {
                   .replace(/&quot;/g, '"')
                   .replace(/&#039;/g, "'"),
                 correct: false,
-                selected: false,
+                isSelected: false,
               })),
               {
                 id: nanoid(),
@@ -41,7 +41,7 @@ function App() {
                   .replace(/&quot;/g, '"')
                   .replace(/&#039;/g, "'"),
                 correct: true,
-                selected: true,
+                isSelected: true,
               },
             ],
           })
--- quizzler/src/Questions.js
+++ quizzler/src/Questions.js
@@ -1,7 +1,14 @@
 export default function Questions(props) {
-  const answerElement = props.answers.map((answer) => (
-    <button key={answer.id}>{answer.answer}</button>
-  ));
+  const answerElement = props.answers.map((answer) => {
+    const styles = {
+      backgroundColor: answer.isSelected ? "#D6DBF5" : "transparent",
+    };
+    return (
+      <button style={styles} key={answer.id}>
+        {answer.answer}
+      </button>
+    );
+  });
   return (
     <div>
       <h3>{props.quizQuestion}</h3>
